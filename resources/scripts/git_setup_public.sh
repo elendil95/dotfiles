3235f62eca70b7e4c92ee21ea13ba3e34b1af991
Elendil95 <uge1995@gmail.com>
2019-03-05 16:18:01 +0100

    moved git setup script to /scripts

---
+++ resources/scripts/git_setup_public.sh
@@ -0,0 +1,4 @@
+git config --global user.name "USER_NAME"
+git config --global user.email"EMAIL"
+git config core.editor "EDITOR" # But really, just use VIM ;)
+git config --global credential.helper store
