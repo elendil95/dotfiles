95a8b83666faea883f6ee371f657624dd27d4944
Elendil <17616744+elendil95@users.noreply.github.com>
2023-10-08 15:11:13 +0200

    moved around stuff in autostart + use more wayland-frindly programs

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-picom -b --config ~/.config/picom/picom.conf &
-wal -R
+picom -b --config ~/.config/picom/picom.conf
+wal R
 xsettingsd &
-# xfce4-power-manager &
-# volumeicon &
-pa-applet &
-nm-applet &
-syncthing-gtk -m &
-redshift-gtk &
-blueman-applet &
 setxkbmap it
 light-locker --lock-on-suspend &
+# xfce4-power-manager &
+#pa-applet
+#volumeicon
+nm-applet &
+syncthing-gtk -m &
+#redshift-gtk
+gammastep-indicator &
+blueman-applet &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+
+if [ "${MOZ_ENABLE_WAYLAND}" = 1 ]; then 
+    export MOZ_ENABLE_WAYLAND=0
+fi
 # pamac-tray-appindicator &
-exec expressvpn connect &
-exit 0
+exec expressvpn connect
